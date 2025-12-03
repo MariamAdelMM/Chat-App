e7042783970512af1eb1f9efa0fb6ce957b9c393
Mariam Adel <mariam.adel@rushbrush.com>
2025-12-03 16:54:30 +0200

    feat:push notifications

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -6,6 +8,7 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
@@ -26,8 +29,31 @@ class ChatMessages extends StatelessWidget {
           padding: const EdgeInsets.only(bottom: 40, left: 13, right: 13),
           reverse: true, //from bottom to top
           itemCount: loadedMessages.length,
-          itemBuilder: (ctx, index) =>
-              Text(loadedMessages[index].data()['text']),
+          itemBuilder: (ctx, index) {
+            // Text(loadedMessages[index].data()['text']),
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+            final currentMessageUserId = chatMessage['username'];
+            final nextMessageUserId = nextChatMessage != null
+                ? nextChatMessage['userId']
+                : null;
+            final nextUserIsSame = nextMessageUserId == currentMessageUserId;
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessage['userImage'],
+                username: chatMessage['username'],
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            }
+          },
         );
       },
     ); //will setup a listener to listen to remote database whenever a new message sent it reads it and trigger this builder and update ui, builder: builder)
